e2a48302365b83a64c3761691594dbc6a1cd0008
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-05-28 17:29:10 +0300

    Refactored db connection to Mongoose.

--- webinar-06-mongoose/model/db.js
+++ webinar-06-mongoose/model/db.js
@@ -1,16 +1,31 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 require('dotenv').config();
-
 const uriDb = process.env.URI_DB;
 
-const db = MongoClient.connect(uriDb, { useNewUrlParser: true, useUnifiedTopology: true, poolSize: 5 });
+const db = mongoose.connect(uriDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  poolSize: 5,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connection open ${uriDb}.`);
+});
+
+mongoose.connection.on('error', error => {
+  console.log(`Error Mongoose connection: ${error.message}.`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log(`Mongoose connection terminated.`);
+});
 
 process.on('SIGINT', async () => {
-  const client = await db;
-  client.close();
-
-  console.log('Connection terminated');
-  process.exit(1);
+  mongoose.connection.close(() => {
+    console.log('Connection to DB terminated.');
+    process.exit(1);
+  });
 });
 
 module.exports = db;

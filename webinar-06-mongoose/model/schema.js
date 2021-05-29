4bc64eb5dac102922e083d5bbbf081c1d15ec479
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-05-29 20:17:59 +0300

    Added virtual field example.

--- webinar-06-mongoose/model/schema.js
+++ webinar-06-mongoose/model/schema.js
@@ -21,8 +21,23 @@ const catSchema = new Schema(
       set: data => (!data ? [] : data),
     },
   },
-  { versionKey: false, timestamps: true }
+  {
+    versionKey: false,
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id;
+        return ret;
+      },
+    },
+    toObject: { virtuals: true },
+  }
 );
+
+catSchema.virtual('info').get(function () {
+  return `${this.name} is ${this.age} years old.`;
+});
 
 const Cat = model('cat', catSchema);
 

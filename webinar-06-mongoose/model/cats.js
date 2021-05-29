d0cf7b1282f22bdf72757290e10f33cf31f59334
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-05-29 14:34:23 +0300

    Refactored methods to work with DB.

--- webinar-06-mongoose/model/cats.js
+++ webinar-06-mongoose/model/cats.js
@@ -1,59 +1,27 @@
-const db = require('./db');
-const { ObjectId } = require('mongodb');
-
-const getCollection = async (db, name) => {
-  const client = await db;
-  const collection = await client.db().collection(name);
-  return collection;
-};
+const Cat = require('./schema');
 
 const getAllCats = async () => {
-  const collection = await getCollection(db, 'cats');
-  const results = await collection.find({}).toArray();
+  const results = await Cat.find();
   return results;
 };
 
 const getCatById = async id => {
-  const collection = await getCollection(db, 'cats');
-  const objId = new ObjectId(id);
-  const [results] = await collection.find({ _id: objId }).toArray();
-  return results;
+  const result = await Cat.findOne({ _id: id });
+  return result;
 };
 
 const removeCat = async id => {
-  const collection = await getCollection(db, 'cats');
-  const objId = new ObjectId(id);
-
-  const { value: result } = await collection.findOneAndDelete({ _id: objId });
-
+  const result = await Cat.findOneAndRemove({ _id: id });
   return result;
 };
 
 const addCat = async body => {
-  const collection = await getCollection(db, 'cats');
-
-  const record = {
-    ...body,
-    ...(body.isVaccinated ? {} : { isVaccinated: false }),
-  };
-
-  const {
-    ops: [result],
-  } = await collection.insertOne(record);
-
+  const result = await Cat.create(body);
   return result;
 };
 
 const updateCat = async (id, body) => {
-  const collection = await getCollection(db, 'cats');
-  const objId = new ObjectId(id);
-
-  const { value: result } = await collection.findOneAndUpdate(
-    { _id: objId },
-    { $set: body },
-    { returnOriginal: false }
-  );
-
+  const result = await Cat.findOneAndUpdate({ _id: id }, { ...body }, { new: true });
   return result;
 };
 

845d3703a78c45478b8c69b71f136c1ed2b04d2e
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-06-02 18:33:24 +0300

    Added initial project files.

---
+++ webinar-07-authentication/repositories/cats.js
@@ -0,0 +1,34 @@
+const Cat = require('../model/schema');
+
+const getAllCats = async () => {
+  const results = await Cat.find();
+  return results;
+};
+
+const getCatById = async id => {
+  const result = await Cat.findOne({ _id: id });
+  return result;
+};
+
+const removeCat = async id => {
+  const result = await Cat.findOneAndRemove({ _id: id });
+  return result;
+};
+
+const addCat = async body => {
+  const result = await Cat.create(body);
+  return result;
+};
+
+const updateCat = async (id, body) => {
+  const result = await Cat.findOneAndUpdate({ _id: id }, { ...body }, { new: true });
+  return result;
+};
+
+module.exports = {
+  getAllCats,
+  getCatById,
+  removeCat,
+  addCat,
+  updateCat,
+};

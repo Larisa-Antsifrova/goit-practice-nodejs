295350f71db084ce6b069cd38c00d521bd604105
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-06-04 18:13:14 +0300

    Added helmet and rate limit.

--- webinar-08-authorization/app.js
+++ webinar-08-authorization/app.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const { apiLimiter } = require('./helpers/constants');
 
 const catsRouter = require('./routes/api/cats/cats');
 const usersRouter = require('./routes/api/users/users');
 
 const app = express();
-
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
+app.use(helmet());
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: 10000 }));
 
+app.use('/api/', rateLimit(apiLimiter));
 app.use('/api/cats', catsRouter);
 app.use('/api/users', usersRouter);
 

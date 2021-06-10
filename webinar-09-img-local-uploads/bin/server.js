443e00c226a2a2497e5010a04d566972a3c8b093
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-06-10 12:41:11 +0300

    Added helper function and logic to create uploads and images folders.

--- webinar-09-img-local-uploads/bin/server.js
+++ webinar-09-img-local-uploads/bin/server.js
@@ -1,10 +1,17 @@
 const app = require('../app');
 const db = require('../model/db');
+const createFolderIfNotExist = require('../helpers/create-folder');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
+const UPLOAD_DIR = process.env.UPLOAD_DIR;
+const AVATAR_DIR = process.env.AVATAR_DIR;
 
 db.then(() => {
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
+    await createFolderIfNotExist(UPLOAD_DIR);
+    await createFolderIfNotExist(AVATAR_DIR);
+
     console.log(`Server running. Use our API on port: ${PORT}`);
   });
 }).catch(error => {

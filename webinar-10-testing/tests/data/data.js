68dc8de9e3b2ad57e2ef8d1a1efcc57f786e043a
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-06-14 22:53:13 +0300

    Added e2e tests to check GET requests.

---
+++ webinar-10-testing/tests/data/data.js
@@ -0,0 +1,24 @@
+// cat to test
+const newCat = { name: "Barsik", age: 5 };
+
+// user to be tested on cats router
+const newUser = {
+  name: "Yarik",
+  gender: "male",
+  email: "yarik@gmail.com",
+  password: "123456",
+};
+
+// usr to be tested on user router
+const newTestUser = {
+  name: "New Yarik",
+  gender: "male",
+  email: "new_yarik@gmail.com",
+  password: "123456",
+};
+
+module.exports = {
+  newCat,
+  newUser,
+  newTestUser,
+};

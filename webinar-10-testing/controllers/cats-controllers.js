0a1351af214fba8f1d1c944b99016a3f22ef6b2d
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-06-14 16:14:25 +0300

    Added test to check updateCat when cat is not found, when repository returns Error.

--- webinar-10-testing/controllers/cats-controllers.js
+++ webinar-10-testing/controllers/cats-controllers.js
@@ -1,10 +1,10 @@
-const Cats = require('../repositories/cats');
+const Cats = require("../repositories/cats");
 
 const getAllCats = async (req, res, next) => {
   try {
     const userId = req.user.id;
     const { docs: cats, ...rest } = await Cats.getAllCats(userId, req.query);
-    return res.json({ status: 'success', code: 200, data: { cats, ...rest } });
+    return res.json({ status: "success", code: 200, data: { cats, ...rest } });
   } catch (error) {
     next(error);
   }
@@ -16,9 +16,9 @@ const getCatById = async (req, res, next) => {
     const id = req.params.id;
     const requiredCat = await Cats.getCatById(userId, id);
     if (requiredCat) {
-      return res.json({ status: 'success', code: 200, cat: requiredCat });
+      return res.json({ status: "success", code: 200, cat: requiredCat });
     }
-    return res.json({ status: 'error', code: 404, message: 'Not found' });
+    return res.json({ status: "error", code: 404, message: "Not found" });
   } catch (error) {
     next(error);
   }
@@ -28,7 +28,7 @@ const addCat = async (req, res, next) => {
   try {
     const userId = req.user.id;
     const cat = await Cats.addCat(userId, req.body);
-    return res.status(201).json({ status: 'success', code: 201, cat });
+    return res.status(201).json({ status: "success", code: 201, cat });
   } catch (error) {
     next(error);
   }
@@ -40,9 +40,18 @@ const removeCat = async (req, res, next) => {
     const id = req.params.id;
     const removedCat = await Cats.removeCat(userId, id);
     if (removedCat) {
-      return res.json({ status: 'success', code: 200, message: 'The cat was deleted', cat: removedCat });
+      return res.json({
+        status: "success",
+        code: 200,
+        message: "The cat was deleted",
+        cat: removedCat,
+      });
     }
-    return res.json({ status: 'success', code: 204, message: 'The cat does not exist' });
+    return res.json({
+      status: "success",
+      code: 204,
+      message: "The cat does not exist",
+    });
   } catch (error) {
     next(error);
   }
@@ -54,9 +63,18 @@ const updateCat = async (req, res, next) => {
     const id = req.params.id;
     const catToUpdate = await Cats.updateCat(userId, id, req.body);
     if (catToUpdate) {
-      return res.json({ status: 'success', code: 200, message: 'The cat was updated', cat: catToUpdate });
+      return res.json({
+        status: "success",
+        code: 200,
+        message: "The cat was updated",
+        cat: catToUpdate,
+      });
     }
-    return res.json({ status: 'error', code: 404, message: 'The cat was not found' });
+    return res.json({
+      status: "error",
+      code: 404,
+      message: "The cat was not found",
+    });
   } catch (error) {
     next(error);
   }
--- webinar-10-testing/tests/unit.cats-controller.test.js
+++ webinar-10-testing/tests/unit.cats-controller.test.js
@@ -11,15 +11,38 @@ describe("Unit testing Cats controllers", () => {
   };
   const next = jest.fn();
 
-  test("updateCat: success", async () => {
+  test("updateCat: cat exists", async () => {
     const updatedCat = { id: 3, name: "Barsik", age: 3 };
     Cats.updateCat = jest.fn(() => {
       return updatedCat;
     });
 
     const result = await updateCat(req, res, next);
+
+    expect(result).toBeDefined();
     expect(result.status).toEqual("success");
     expect(result.code).toEqual(200);
     expect(result.cat).toEqual(updatedCat);
+  });
+
+  test("updateCat: cat not found", async () => {
+    Cats.updateCat = jest.fn();
+
+    const result = await updateCat(req, res, next);
+
+    expect(result).toBeDefined();
+    expect(result.status).toEqual("error");
+    expect(result.code).toEqual(404);
+    expect(result.message).toEqual("The cat was not found");
+  });
+
+  test("updateCat: error handler", async () => {
+    Cats.updateCat = jest.fn(() => {
+      throw new Error("Some error occured!");
+    });
+
+    await updateCat(req, res, next);
+
+    expect(next).toHaveBeenCalled();
   });
 });
